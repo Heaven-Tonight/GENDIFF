60496755a1e6155af6c81e5f7f5f696332206a0a
Svetlna <svetlana@dev.com>
2023-10-10 17:06:50 +0300

    modified showInfo.js

--- showInfo.js
+++ showInfo.js
@@ -5,8 +5,10 @@ const program = new Command();
 const showInfo = () => {
   program
     .description('Compares two configuration files and shows a difference.')
+    .arguments('filepath1 filepath2')
     .helpOption('-h, --help', 'output usage information')
-    .option('-V, --version ', 'output the version number');
+    .option('-V, --version ', 'output the version number')
+    .option('-f, --format <type>', 'output format');
 
   program.parse();
 
