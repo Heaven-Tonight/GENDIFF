3290efea9c5fa1d5c616f9f813ee4e221bb54208
Svetlna <svetlana@dev.com>
2023-10-11 16:44:55 +0300

    updated parse.js

--- parse.js
+++ parse.js
@@ -1,15 +1,15 @@
 import path from 'path';
 import { readFileSync } from 'node:fs';
+import genDiff from './index.js';
 
 const __dirname = path.resolve();
 
 const map = {
-  json: (path) => JSON.parse(path),
-  // yml: (path) => // здесь д.б. yaml парсер
+  json: (filepath) => JSON.parse(filepath),
+  // yml: (filepath) => // здесь д.б. yaml парсер
 };
 
 const getFiletype = (filepath) => filepath.split('.')[1];
-
 
 const parse = (filepath1, filepath2) => {
   const type1 = getFiletype(filepath1);
@@ -18,7 +18,7 @@ const parse = (filepath1, filepath2) => {
   const path2 = path.resolve(__dirname, 'files', `file${2}.${type2}`);
   const data1 = map[type1](readFileSync(path1));
   const data2 = map[type2](readFileSync(path2));
-  console.log(data1, '\n', data2)
+  genDiff(data1, data2);
 };
 
 export default parse;
