1f2ac1c0dd38d951abba0a231d994dcec5f65df3
Svetlna <svetlana@dev.com>
2023-10-12 21:15:41 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,36 +1,5 @@
-import _ from 'lodash';
-import path from 'path';
-import readFile from './helpers/read.js';
-import parse from './helpers/parse.js';
+// @ts-check
 
-const genDiff = (filepath1, filepath2) => {
-  const [data1, data2] = [readFile(filepath1), readFile(filepath2)];
-  const [ext1, ext2] = [path.extname(filepath1), path.extname(filepath2)];
-  const [parsed1, parsed2] = [parse(data1, ext1), parse(data2, ext2)];
-  const keys = Object.keys({ ...parsed1, ...parsed2 });
-  const sortedKeys = _.orderBy(keys);
-  const result = [];
-  sortedKeys.forEach((key) => {
-    const value1 = parsed1[key];
-    const value2 = parsed2[key];
-    if (!_.has(parsed1, key)) {
-      result.push(` + ${key}: ${parsed2[key]}`);
-    }
-    if (!_.has(parsed2, key)) {
-      result.push(` - ${key}: ${parsed1[key]}`);
-    }
-    if (_.has(parsed2, key) && _.has(parsed1, key)) {
-      if (value1 !== value2) {
-        result.push(
-          ` - ${key}: ${parsed1[key]}`,
-          ` + ${key}: ${parsed2[key]}`,
-        );
-      } else {
-        result.push(`   ${key}: ${parsed1[key]}`);
-      }
-    }
-  });
-  return `{\n${result.join('\n')}\n}`;
-};
+import half from './src/half.js';
 
-export default genDiff;
+export default half;
