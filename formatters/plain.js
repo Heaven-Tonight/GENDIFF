e7acdecf7adc42797d6e1e198ab940d8d00adeea
Svetlna <svetlana@dev.com>
2023-10-20 19:08:14 +0300

    updated plain.js

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,29 +1,28 @@
-import buildChildrenList from "../graphs/list.js";
-import buildPath from "../graphs/path.js";
+import buildChildrenList from '../graphs/list.js';
+import buildPath from '../graphs/path.js';
 
 const formatValue = (value) => {
   if (Array.isArray(value)) {
     return value
-      .map((item) => (typeof item === 'object' && item !== null) ? '[complex value]' : `'${item}'`);
+      .map((item) => (typeof item === 'object' && item !== null ? '[complex value]' : `'${item}'`));
   }
   if (typeof value === 'object') {
     return '[complex value]';
   }
-  if (typeof value === 'string' || typeof value === null) {
+  if (typeof value === 'string' || value === null) {
     return `'${value}'`;
   }
   return value;
 };
 
-export default (obj) => {
-
+const plain = (obj) => {
   const childrenList = buildChildrenList(obj);
 
   const iter = (data) => {
     const keys = Object.keys(data);
 
-    return keys.flatMap((key, i) => {
-      const {children = [], status } = data[key];
+    return keys.flatMap((key) => {
+      const { children = [], status } = data[key];
       const path = buildPath(childrenList, key);
       if (children.length === 0) {
         const { value } = data[key];
@@ -36,7 +35,7 @@ export default (obj) => {
             return `Property '${path}' was added with value: ${formattedValue}`;
           }
           case 'changed': {
-            return `Property '${path}' was updated. From ${formattedValue[0]} to ${formattedValue[1]}`
+            return `Property '${path}' was updated. From ${formattedValue[0]} to ${formattedValue[1]}`;
           }
           case 'unchanged': {
             break;
@@ -49,3 +48,5 @@ export default (obj) => {
   };
   return iter(obj).join('\n');
 };
+
+export default plain;
