18f87dc3aafe025059cdabc2d8a68ce42dfe0e8c
Svetlna <svetlana@dev.com>
2023-10-20 19:06:28 +0300

    updated helpers/diff.js

--- helpers/diff.js
+++ helpers/diff.js
@@ -1,27 +1,25 @@
 import _ from 'lodash';
 
-const getDiff = (file1, file2) => {
+const buildDifference = (file1, file2) => {
   const keys = Object.keys({ ...file1, ...file2 });
   const sortedKeys = _.orderBy(keys);
   return sortedKeys.reduce((acc, key) => {
     const value1 = file1[key];
     const value2 = file2[key];
     if (_.isObject(value1) && _.isObject(value2)) {
-      return { ...acc, [`  ${key}`]: getDiff(value1, value2) };
-    } else {
-      if (value2 === undefined) {
-        return { ...acc, [`- ${key}`]: value1 };
-      }
-      if (value1 === undefined) {
-        return { ...acc, [`+ ${key}`]: value2 };
-      } else {
-        if (value1 === value2) {
-          return { ...acc, [`  ${key}`]: value1 };
-        }
-        return { ...acc, [`- ${key}`]: value1, [`+ ${key}`]: value2 };
-      }
+      return { ...acc, [key]: { children: [buildDifference(value1, value2)], status: 'unchanged' } };
     }
+    if (value2 === undefined) {
+      return { ...acc, [key]: { value: value1, status: 'removed' } };
+    }
+    if (value1 === undefined) {
+      return { ...acc, [key]: { value: value2, status: 'added' } };
+    }
+    if (value1 === value2) {
+      return { ...acc, [key]: { value: value1, status: 'unchanged' } };
+    }
+    return { ...acc, [key]: { value: [value1, value2], status: 'changed' } };
   }, {});
 };
 
-export default getDiff;
+export default buildDifference;
