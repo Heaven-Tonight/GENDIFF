0f0f55d1d786c2d4e8ae9e36de2842f1f60aeebc
Svetlna <svetlana@dev.com>
2023-10-20 19:07:38 +0300

    updated graphs/list.js

--- graphs/list.js
+++ graphs/list.js
@@ -1,13 +1,18 @@
-const buildChildrenList = (obj, parent = null) => {
-  return Object.keys(obj).reduce((acc, key) => {
+const buildChildrenList = (obj, parent = null) => Object.keys(obj)
+  .reduce((acc, key) => {
     const { children } = obj[key];
     if (children === undefined) {
-      return { ...acc, [key]: [parent, []] };
+      return {
+        ...acc,
+        [key]: [parent, []],
+      };
     }
     const list = children.flatMap((item) => Object.keys(item));
-    return { ...acc,  [key]: [ parent, list ], ...children.reduce((acc, value) =>
-        ( { ...acc, ...buildChildrenList(value, key) }), {}) }
+    return {
+      ...acc,
+      [key]: [parent, list],
+      ...children.reduce((accum, value) => ({ ...accum, ...buildChildrenList(value, key) }), {}),
+    };
   }, {});
-};
 
 export default buildChildrenList;
