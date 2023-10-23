44dc521b0c2179fbae5256a184e218a8eac0fcfb
Svetlna <svetlana@dev.com>
2023-10-24 01:14:16 +0300

    updated read.js

--- helpers/read.js
+++ helpers/read.js
@@ -1,17 +1,3 @@
 import { readFileSync } from 'node:fs';
-import { fileURLToPath } from 'url';
-import path from 'path';
 
-const getFixturePath = (file) => {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  const ext = path.extname(file);
-  const basename = path.basename(file, ext);
-  return path.resolve(__dirname, '..', '__fixtures__', `file${basename.slice(-1)}${ext}`);
-};
-
-const readFile = (file) => {
-  const filepath = getFixturePath(file);
-  return readFileSync(filepath, 'utf-8');
-};
-
-export default readFile;
+export default (filepath) => readFileSync(filepath, 'utf-8');
