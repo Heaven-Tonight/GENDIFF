80552fafcf5114fdc7a00cb50e07a2db8b9f6a0e
Svetlna <svetlana@dev.com>
2023-10-23 18:24:23 +0300

    updated path.js

--- helpers/path.js
+++ helpers/path.js
@@ -1,10 +1,9 @@
 import path from 'path';
 
 const getFixturePath = (file) => {
-  const __dirname = path.resolve();
   const ext = path.extname(file);
   const basename = path.basename(file, ext);
-  return path.resolve(__dirname, '__fixtures__', `${ext.slice(1)}`, `file${basename.slice(-1)}${ext}`);
+  return path.join(process.cwd(), '__fixtures__', `${ext.slice(1)}`, `file${basename.slice(-1)}${ext}`);
 };
 
 export default getFixturePath;
