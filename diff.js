7194d991a3eead5b65900c3ff826ed0f18026f33
Svetlna <svetlana@dev.com>
2023-10-24 01:14:39 +0300

    updated diff.js

--- diff.js
+++ diff.js
@@ -1,15 +1,19 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
 import buildDifference from './helpers/diff.js';
 import readFile from './helpers/read.js';
 import parse from './helpers/parse.js';
 import formatters from './formatters/index.js';
 
+const getFilePath = (filepath) => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  return path.resolve(__dirname, filepath);
+};
+
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const [data1, data2] = [readFile(filepath1), readFile(filepath2)];
+  const [data1, data2] = [readFile(getFilePath(filepath1)), readFile(getFilePath(filepath2))];
   const [ext1, ext2] = [path.extname(filepath1), path.extname(filepath2)];
   const [parsed1, parsed2] = [parse(data1, ext1), parse(data2, ext2)];
-  console.log('file1', JSON.stringify(parsed1, null, ' '));
-  console.log('file2', JSON.stringify(parsed2, null, ' '));
   const diff = buildDifference(parsed1, parsed2);
   const formatted = formatters[format](diff);
   return formatted;
