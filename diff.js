7c4d681579ef64379ec983c32415d0cc79a70ce0
Svetlna <svetlana@dev.com>
2023-10-24 13:20:41 +0300

    updated diff.js

--- diff.js
+++ diff.js
@@ -14,8 +14,10 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const [data1, data2] = [readFile(getFilePath(filepath1)), readFile(getFilePath(filepath2))];
   const [ext1, ext2] = [path.extname(filepath1), path.extname(filepath2)];
   const [parsed1, parsed2] = [parse(data1, ext1), parse(data2, ext2)];
-  console.log(parsed1);
-  console.log(parsed2);
+  console.log(data1);
+  console.log(data2);
+  console.log(JSON.stringify(parsed1, null, ' '))
+  console.log(JSON.stringify(parsed2, null, ' '))
   const diff = buildDifference(parsed1, parsed2);
   const formatted = formatters[format](diff);
   return formatted;
