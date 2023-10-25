5eb16a93706e766f5ce07c74e96ccaf4adfa77f0
Svetlna <svetlana@dev.com>
2023-10-25 22:05:03 +0300

    updated diff.js

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,6 +1,3 @@
-import buildChildrenList from '../graphs/list.js';
-import buildPath from '../graphs/path.js';
-
 const formatValue = (value) => {
   if (Array.isArray(value)) {
     return value.map((val) => formatValue(val));
@@ -17,38 +14,33 @@ const formatValue = (value) => {
   return value;
 };
 
-const plain = (obj) => {
-  const childrenList = buildChildrenList(obj);
-
-  const iter = (data) => {
-    const keys = Object.keys(data);
-
-    return keys.flatMap((key) => {
-      const { children = [], status } = data[key];
-      const path = buildPath(childrenList, key);
-      if (children.length === 0) {
-        const { value } = data[key];
-        const formattedValue = formatValue(value);
-        switch (status) {
-          case 'removed': {
-            return `Property '${path}' was removed`;
-          }
-          case 'added': {
-            return `Property '${path}' was added with value: ${formattedValue}`;
-          }
-          case 'changed': {
-            return `Property '${path}' was updated. From ${formattedValue[0]} to ${formattedValue[1]}`;
-          }
-          case 'unchanged': {
-            break;
-          }
-          default: return `Unexpected status - ${status}`;
-        }
+const helper = (data, path) => Object.keys(data).flatMap((key) => {
+  const { children = [], status } = data[key];
+  if (children.length === 0) {
+    const { value } = data[key];
+    const formattedValue = formatValue(value);
+    switch (status) {
+      case 'removed': {
+        return `Property '${[...path, key]
+          .join('.')}' was removed`;
       }
-      return children.flatMap((item) => iter(item));
-    });
-  };
-  return iter(obj).join('\n');
-};
+      case 'added': {
+        return `Property '${[...path, key]
+          .join('.')}' was added with value: ${formattedValue}`;
+      }
+      case 'changed': {
+        return `Property '${[...path, key]
+          .join('.')}' was updated. From ${formattedValue[0]} to ${formattedValue[1]}`;
+      }
+      case 'unchanged': {
+        break;
+      }
+      default: return `Unexpected status - ${status}`;
+    }
+  }
+  return children.flatMap((child) => helper(child, [...path, key]));
+});
+
+const plain = (obj) => helper(obj, []).join('\n');
 
 export default plain;
